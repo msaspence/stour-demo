de3baceebcd8a5e0732ec41b543e7d1d6dfeb89b
Matthew Spence <msaspence@gmail.com>
2012-05-14 10:43:04 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Werehiring::Application.config.secret_token = 'b92cecfae1282d6a25e65b575d8fcbe908f463efb126d93e510439f68f8de2c0afb509fbfac68b0fefa7401f9c18c305664caaf264cbeed57f3304210f20a6ec'
